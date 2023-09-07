5c083388b4b4e84435554bbd44671b8fdd32a506
NahomGebeyehu <gebeyehuay@gmail.com>
2023-09-07 16:23:29 +0300

    second

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - initial point
+ *
+ * Return: 0 return
+ */
+int main(void)
+{
+	int i;
+	long int l;
+	long long int d;
+	char c;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
