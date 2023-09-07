1ef945bc17240c732aeb2ce2a97dae9d6fc70033
NahomGebeyehu <gebeyehuay@gmail.com>
2023-09-07 16:58:04 +0300

    second

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, strlen(msg));
-	fwrite(msg, sizeof(char), strlen(msg), stderr);
+
+	write(STDERR_FILENO, msg,
+strlen(msg));
 	return (1);
 }
